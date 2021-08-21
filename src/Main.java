ff666d1d9d6829983b29fedeb65d081e71c93aec
Amanoj3 <amanoj3@uic.edu>
2021-08-20 21:53:17 -0500

    Added an alert box which displays the rows from the sample table

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
 import java.sql.*;
+import java.util.ArrayList;
 import javax.swing.*;
 
 public class Main { // Citation -  I used this link as a reference:
     // https://www.javatpoint.com/example-to-connect-to-the-mysql-database
     public static void main(String[] args) {
-        System.out.println("hey");
+        ArrayList<String> queryStrings = new ArrayList<String>();
+        StringBuilder alertBoxMessage = new StringBuilder();
         Connection con = null;
         try {
             System.out.println("heeee"); // works up until this point
@@ -13,7 +15,6 @@ public class Main { // Citation -  I used this link as a reference:
             String query = "select * from sampletable";
             Statement st = con.createStatement();
             ResultSet result = st.executeQuery(query);
-            int index = 1;
             while (result.next()) {
                 String currentString = "";
                 currentString += result.getInt("sampleId");
@@ -21,9 +22,15 @@ public class Main { // Citation -  I used this link as a reference:
                 currentString += result.getString("sampleName");
                 currentString += " ";
                 currentString += result.getString("sampleDate");
+                queryStrings.add(currentString);
                 System.out.println(currentString);
             }
             con.close();
+            alertBoxMessage.append(" ID| NAME| DATE").append("\n");
+            for (String queryString : queryStrings) {
+                alertBoxMessage.append(queryString).append("\n");
+            }
+            JOptionPane.showMessageDialog(null, alertBoxMessage);
         }
         catch(SQLException | ClassNotFoundException ex) {
             System.out.println("Exception caught gracefully!");
